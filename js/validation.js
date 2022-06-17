1f0f1cc572e6486dc3154ff7b8c0befed273c37d
Principe Matias <principematias23@gmail.com>
2022-06-16 21:14:16 -0300

    Cargando modificacion boton menu hamburguesa con animacion

--- js/validation.js
+++ js/validation.js
@@ -4,6 +4,9 @@
 const menuHamburguesa = document.getElementById('menu');
 menuHamburguesa.addEventListener('click', () => {
     document.getElementById('navLinks').classList.toggle('nav__link--action');
+    document.getElementById('lineOne').classList.toggle('nav__img-linea--1');
+    document.getElementById('lineTwo').classList.toggle('nav__img-linea--2');
+    document.getElementById('lineThree').classList.toggle('nav__img-linea--3');
 });
 
 //ValidationFormulario
